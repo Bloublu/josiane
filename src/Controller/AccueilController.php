f0b28b27ca1ef9a68f9954215e14b9e0cd0604e6
bastien <bastien.benariac@gmail.com>
2021-12-19 22:34:15 +0100

    page en construction

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,10 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
-    #[Route('/', name: 'Accueil')]
+    /**
+     * @Route("/", name="accueil")
+     */
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    /**
+     * @Route("travaux", name="travaux")
+     */
+    public function travaux(): Response
+    {
+        return $this->render('accueil/travaux.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
